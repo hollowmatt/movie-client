bc3c0f30bc3f99d8b507ee3459578c22f4a1e0ec
Matt Holloway <hollowmatt@gmail.com>
2022-03-29 18:52:38 -0400

    insert movie functionality added

--- src/pages/MoviesInsert.jsx
+++ src/pages/MoviesInsert.jsx
@@ -35,11 +35,81 @@ const CancelButton = styled.a.attrs({
 `;
 
 class MoviesInsert extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            rating: '',
+            time: '',
+        };
+    }
+
+    handleChangeInputName = async event => {
+        const name = event.target.value;
+        this.setState({name});
+    }
+
+    handleChangeInputRating = async event => {
+        const rating = event.target.validity.valid ? event.target.value : this.state.rating;
+        this.setState({rating});
+    }
+
+    handleChangeInputTIme = async event => {
+        const time = event.target.value;
+        this.setState({time});
+    }
+
+    handleIncludeMovie = async () => {
+        const {name, rating, time} = this.state;
+        const arrayTime = time.split(' / ');
+        const payload = {name, rating, time: arrayTime};
+
+        await api.insertMovie(payload).then(res => {
+            window.alert('Movie inserted successfully');
+            this.setState({
+                name: '',
+                rating: '',
+                time: '',
+            });
+        })
+    }
+
     render() {
+        const {name, rating, time} = this.state;
         return (
-            <div>
-                <p> insert movie stuff will go here</p>
-            </div>
+            <Wrapper>
+                <Title>Create Movie</Title>
+
+                <Label>Name: </Label>
+                <InputText
+                    type="text"
+                    value={name}
+                    onChange={this.handleChangeInputName}
+                />
+
+                <Label>Rating: </Label>
+                <InputText
+                    text="number"
+                    step="0.1"
+                    lang="en-us"
+                    min="0"
+                    max="10"
+                    pattern="[0-9+([,\.][0-9]+)?"
+                    value={rating}
+                    onChange={this.handleChangeInputRating}
+                />
+
+                <Label>Time: </Label>
+                <InputText
+                    type="text"
+                    value={time}
+                    onChange={this.handleChangeInputTIme}
+                />
+
+                <Button onClick={this.handleIncludeMovie}>Add Movie</Button>
+                <CancelButton href={'/movies/list'}>Cancel</CancelButton>
+            </Wrapper>
         );
     }
 }
